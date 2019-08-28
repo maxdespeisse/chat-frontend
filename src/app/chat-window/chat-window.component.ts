9ae5d6443139126dc5c1c3e7904d730eafbde95e
Maxime Despeisse <maxime.despeisse@gmail.com>
2019-08-28 17:00:13 +0200

    Add scrolling on new message

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -10,18 +10,32 @@ export class ChatWindowComponent implements OnInit {
 
   messages = [];
   newMessage: string;
+  shouldScrollToBottom: boolean = false;
+  firstLoad: boolean = false;
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit() {
-    this.messageService.getPreviousMessages().subscribe(messages => this.messages = messages);
+    this.messageService.getPreviousMessages().subscribe(messages => {
+      this.messages = messages;
+      this.shouldScrollToBottom = true;
+    });
 
     this.messageService.getNewMessage().subscribe(message => {
       this.messages.push(message);
+      this.shouldScrollToBottom = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight;
     });
   }
 
   send() {
     this.messageService.send(this.newMessage);
   }
+
+  ngAfterViewChecked() {
+    if (this.shouldScrollToBottom) {
+      window.scrollTo(0, document.body.scrollHeight);
+      this.shouldScrollToBottom = false;
+    }
+  }
+
 }
